700e9447d3cb09ae6053063e81c76a0c877b5f15
choumanhussein <chouman.hussein7@gmail.com>
2022-03-12 06:55:11 +0000

    Seed&&Dic

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'mini_magick'
 gem 'jbuilder', '~> 2.7'
 gem 'rexml'
 
-
+gem 'faker' 
 
 group :development, :test do
 
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ module InstaApp
   class Application < Rails::Application
     config.time_zone = 'GMT'
     config.active_record.default_timezone = :local
+    I18n.available_locales = [:en, :fr]
     config.i18n.default_locale = :fr
     config.load_defaults 6.0
 
